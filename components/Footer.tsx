b192e67971d773b1f185bf6f0df47f915bd9aba1
mahbub alam <ma01776879@gmail.com>
2023-11-22 14:02:45 +0600

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,40 @@
+import Image from 'next/image'
 import React from 'react'
+
+const FooterColumn = () =>{
+  <div className='footer_column'>
+    <h4 className='font-semibold'>
+   Title
+    </h4>
+    <ul className='flex flex-col gap-2 font-normal'>
+      Links
+    </ul>
+
+  </div>
+}
+
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='flexStart footer'>
+      <div className='flex flex-col gap-12 w-full'>
+        <div className='flex items-start flex-col'>
+          <Image src={'/logo-purple.svg'} width={115} height={43} alt='flexible'/>
+          <p className='text-start text-sm font-normal mt-5 max-w-xs'>
+          Flexibble is the world&apos;s leading community for creatives to share, grow, and get hired.
+          </p>
+        </div>
+
+        <div className='flex flex-wrap gap-10'>
+          {/* <FooterColumn /> */}
+
+        </div>
+
+        
+
+      </div>
+
+    </footer>
   )
 }
 
