8a68521a65d8feaa7a5eecde7dadca8aa0cb8461
mahbub alam <ma01776879@gmail.com>
2023-11-29 15:45:12 +0600

    add auth-section

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { sign } from "crypto"
 import { getProviders,signIn } from "next-auth/react"
 import { useState,useEffect } from "react"
 
@@ -15,12 +16,24 @@ type Providers=Record<string,Provider>;
 const AuthProviders = () => {
   const[providers,setProviders]=useState<Providers | null>(null)
 
+  useEffect(()=>{
+ const fetchProviders = async () =>{
+  const res = await getProviders();
+  setProviders(res)
+  console.log(res,'============')
+ }
+ fetchProviders()
+  },[])
+
   if(providers){
     return (
       <div> 
         {
           Object.values(providers).map((provider:Provider,i)=>(
-            <button>{provider?.id}</button>
+            // <button key={i}>{provider?.id}</button>
+            <button className="transition ease-in-out delay-150 h-[40px] w-[150px] shadow-md rounded-sm p-2 text-white bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 " onClick={()=>signIn(provider?.id)}>
+              Sign In
+            </button>
           ))
 
       }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,11 +3,14 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import AuthProviders from './AuthProviders'
+import { getCurrentUser } from '@/lib/session'
 
 
-const Navbar = () => {
+const Navbar = async () => {
 
-const session={}
+
+const session= await getCurrentUser()
+console.log(session?.user?.id,'session======')
   return (
     <nav className='flexBetween navbar'>
         <div className='flex-1 flexStart gap-10'>
@@ -25,8 +28,11 @@ const session={}
 
         <div className='flexCenter gap-4'>
             {
-                session ? (<>
-                UserPhoto
+                session?.user ? (<>
+                {
+                    session?.user?.image &&  (<Image src={session?.user?.image} height={40} width={40} className='rounded-full' alt='session.user.image'/>)
+                }
+              
                 <Link href={'/create-project'}>
                 share Work</Link>
                 </>):(<>
